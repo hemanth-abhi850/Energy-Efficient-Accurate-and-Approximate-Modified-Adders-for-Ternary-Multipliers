65d45dcacb4b4af1814ca73f67a4f621d003a5bc
L Hemanth Krishna <lavatihemanthkrishna@gmail.com>
2024-11-17 08:36:52 +0530

    Create Decimal-2-Ternary.py

---
+++ Python/Decimal-2-Ternary.py
@@ -0,0 +1,17 @@
+def DTC6(pxl):
+
+    """ Input : Decimal Value ; Output : Ternary string """
+
+    tern = [0]*6
+    value = [3**(5 - i) for i in range(6)]
+
+    for i in range(6):
+
+        if pxl >= value[i]:
+            tern[i] = pxl // value[i]
+            pxl = pxl % value[i]
+
+    tern = [str(i) for i in tern]
+    tern = "".join(tern)
+
+    return tern
