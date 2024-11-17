ca53768fe4b3b66bc79cce6091ff67152800a04e
L Hemanth Krishna <lavatihemanthkrishna@gmail.com>
2024-11-17 08:38:02 +0530

    Update Ternary-2-Decimal.py

--- Python/Ternary-2-Decimal.py
+++ Python/Ternary-2-Decimal.py
@@ -1,17 +1,9 @@
-def Ternary(pxl):
+def decimal(n):
 
-    """ Input : Decimal Value ; Output : Ternary string """
+  k = list(n)
+  sum = 0
+  k.reverse()
+  for i in range(len(k)):
+      sum = sum + (int(k[i])* (3**i))
+  return sum
 
-    tern = [0]*6
-    value = [3**(5 - i) for i in range(6)]
-
-    for i in range(6):
-
-        if pxl >= value[i]:
-            tern[i] = pxl // value[i]
-            pxl = pxl % value[i]
-
-    tern = [str(i) for i in tern]
-    tern = "".join(tern)
-
-    return tern
